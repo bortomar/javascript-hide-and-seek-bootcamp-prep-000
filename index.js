eaf29845ad0750cd147ff1551c9bb78d4e269679
Mario Bortolotti <bortomar@email.cz>
2018-06-08 10:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget() {
+  document.querySelector('#nested').removeChild(document.querySelector('#nested .target'));
+}
