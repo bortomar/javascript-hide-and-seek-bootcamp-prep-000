4be0639c8702df87abf43f289f0127c4dfaa82f9
Mario Bortolotti <bortomar@email.cz>
2018-06-08 10:25:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   document.querySelector('#nested .target').parentNode.removeChild(document.querySelector('#nested .target'));
 }
+
+function deepestChild(){
+  var children = document.querySelectorAll('#grand-node *');
+  children[children.length-1].parentNode.removeChild(children[children.length-1]);
+}
